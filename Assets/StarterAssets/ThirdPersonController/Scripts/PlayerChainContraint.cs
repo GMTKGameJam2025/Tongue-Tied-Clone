360ec67157293b11a64cdbad4cc07133dd41f918
TheDLCrimson <duclnc2004@gmail.com>
2025-07-28 18:55:53 +0700

    [FEAT] aDDED REALLY BASIC CONSTRAINT

--- Assets/StarterAssets/ThirdPersonController/Scripts/PlayerChainContraint.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/PlayerChainContraint.cs
@@ -1,16 +1,57 @@
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerChainContraint : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Chain Settings")]
+    public Transform anchorPoint;
+    public float chainLength = 5.0f;
+    public bool chainEnabled = true;
+
+    [Header("Visual")]
+    public bool showChain = true;
+    public float chainWidth = 0.05f;
+    public Material chainMaterial;
+
+    private CharacterController _controller;
+    private LineRenderer _line;
+
     void Start()
     {
-        
+        _controller = GetComponent<CharacterController>();
+
+        if (showChain)
+        {
+            _line = gameObject.AddComponent<LineRenderer>();
+            _line.positionCount = 2;
+            _line.startWidth = chainWidth;
+            _line.endWidth = chainWidth;
+            _line.material = chainMaterial ?? new Material(Shader.Find("Sprites/Default"));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if (!chainEnabled || anchorPoint == null) return;
+
+        Vector3 offset = transform.position - anchorPoint.position;
+
+        if (offset.magnitude > chainLength)
+        {
+            Vector3 clampedPos = anchorPoint.position + offset.normalized * chainLength;
+            Vector3 correction = clampedPos - transform.position;
+            _controller.Move(correction);
+        }
+
+        if (showChain && _line != null)
+        {
+            _line.enabled = true;
+            _line.SetPosition(0, anchorPoint.position);
+            _line.SetPosition(1, transform.position);
+        }
+        else if (_line != null)
+        {
+            _line.enabled = false;
+        }
     }
 }
